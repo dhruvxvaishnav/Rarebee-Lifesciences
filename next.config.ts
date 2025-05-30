6c80cb5e902bb6f9f10f0031a256744878b68b9f
dhruvxvaishnav <dhruvvaishnav687@gmail.com>
2025-05-30 11:14:25 +0530

    Minor Changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    forceSwcTransforms: true,
-  },
-}
-module.exports = nextConfig
+const nextConfig = {};
+module.exports = nextConfig;
