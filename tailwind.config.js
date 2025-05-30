eb574cbd013f8a24730d097914cf36d01c9c4593
dhruvxvaishnav <dhruvvaishnav687@gmail.com>
2025-05-30 11:07:27 +0530

    Fix: Tailwind Config File Creation

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        rarebee: {
+          bg: "#F8FAFC",
+          primary: "#D9EAFD",
+          secondary: "#BCCCDC",
+          text: "#9AA6B2",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
